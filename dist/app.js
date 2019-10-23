197c5201388b7548006fe1b9c71795bb78123b4c
Lucas Amaral <lukas@hotmail.co.jp>
2019-10-23 19:17:54 -0300

    envs

--- dist/app.js
+++ dist/app.js
@@ -13,17 +13,25 @@ const mongoose = require("mongoose");
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema_1 = require("./graphql/schema");
+const dotenv_1 = require("dotenv");
+const path_1 = require("path");
+const env = process.env.NODE_ENV || "prod";
+const configPath = path_1.join(__dirname, './config', `.env.${env}`);
+dotenv_1.config({
+    path: configPath
+});
 const app = express();
 exports.app = app;
+console.log("ENV>", process.env.MONGODB_URL);
 const connectMongo = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield mongoose.connect("mongodb://localhost:27017/cbm", {
+    yield mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true
     });
 });
 const startServer = () => {
     app.use("/graphql", graphqlHTTP({
         schema: schema_1.default,
-        graphiql: process.env.NODE_ENV === "development"
+        graphiql: process.env.NODE_ENV === "dev"
     }));
     connectMongo();
 };
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,24 @@ import * as mongoose from "mongoose";
 import * as express from "express";
 import * as graphqlHTTP from "express-graphql";
 import schema from "./graphql/schema";
+import { config } from "dotenv";
+import { join } from "path";
+
+const env = process.env.NODE_ENV || "prod";
+
+const configPath = join(__dirname, './config', `.env.${env}`)
+
+config({
+  path: configPath
+})
 
 const app = express();
 
+
+console.log("ENV>", process.env.MONGODB_URL)
+
 const connectMongo = async () => {
-  await mongoose.connect("mongodb://localhost:27017/cbm", {
+  await mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true
   });
 };
@@ -16,7 +29,7 @@ const startServer = () => {
     "/graphql",
     graphqlHTTP({
       schema: schema,
-      graphiql: process.env.NODE_ENV === "development"
+      graphiql: process.env.NODE_ENV === "dev"
     })
   );
   connectMongo();
